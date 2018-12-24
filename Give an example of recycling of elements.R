d2361ee71c0ad82f30ba66d4c0635d86f408969d
shillarmpofu <mpofushillar@gmail.com>
2018-12-24 10:03:29 +0200

    Add files via upload

---
+++ Give an example of recycling of elements.R
@@ -0,0 +1,4 @@
+c(51,53,55,57) + c(51,55)
+#vector c(51,55) has reapeted itself to form c(51,55,51,55)
+#if you then run line 4 it should give the same result as line 1
+c(51,53,55,57) + c(51,55,51,55)
